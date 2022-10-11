f4a4e48f7ac3ec22e233d79c0b3f22dfb29fb2c8
이수정 <tidh1640@naver.com>
2022-10-11 11:03:47 +0900

    다형성 실습.

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
@@ -1,15 +1,8 @@
 package com.nhnacademy.edu.springframework.messagesender;
 
 public class Main {
-    public static void sendSmsMessage(User user, String message) {
-        System.out.println("SMS Message Sent to ${phoneNumber} : ${message}");
-    }
-
-    public static void sendEmailMessage(User user, String message) {
-        System.out.println("Email Message Sent to ${email} : ${message}");
-    }
-
     public static void main(String args[]) {
-        sendSmsMessage(new User("email@google.com", "010-1234-5678"), "Hello!!");
+        new MessageSendService(new SmsMessageSender()).doSendMessage();
+        new MessageSendService(new EmailMessageSender()).doSendMessage();
     }
 }
